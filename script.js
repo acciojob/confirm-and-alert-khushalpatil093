426befc9a4ffa85222bd99bbe486acb7b4fa0b02
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-05-20 22:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let x = confirm("Do you want to proceed?");
+
+if (x === true) {
+	alert("You clicked OK. Proceeding...");
+} else {
+	alert("You clicked Cancel. Exiting...");
+}
